1194e560d76c24f80cf0b0fd9e36fc37a413cdcc
Gil Andrey <gil_andrey@hotmail.com>
2024-09-02 16:34:17 -0300

    Connecting MongoDB with repository and service

---
+++ src/main/java/com/gilandrey/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gilandrey.workshopmongo.repository;
+
+import com.gilandrey.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
